e2c017693321a638291f11fbab478afaa90bd433
jin <kyuj0810@gmail.com>
2023-06-12 14:30:07 +0900

    architecture part two

--- server.js
+++ server.js
@@ -1,51 +1,9 @@
-import { PrismaClient } from '@prisma/client';
 import { ApolloServer, gql } from 'apollo-server';
 import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core';
-
-const client = new PrismaClient();
-
-const typeDefs = gql`
-  type Movie {
-    id: Int!
-    title: String!
-    year: Int!
-    genre: String
-    createdAt: String!
-    updatedAt: String!
-  }
-  type Query {
-    movies: [Movie]
-    movie(id: Int!): Movie
-  }
-  type Mutation {
-    createMovie(title: String!, year: Int!, genre: String): Movie
-    deleteMovie(id: String!): Boolean
-  }
-`;
-
-const resolvers = {
-  Query: {
-    movies: () => client.movie.findMany(),
-    movie: (_, { id }) => ({ title: 'Hello', year: 2021 }),
-  },
-  Mutation: {
-    createMovie: (_, { title, year, genre }) =>
-      client.movie.create({
-        data: {
-          title,
-          year,
-          genre,
-        },
-      }),
-    deleteMovie: (_, { id }) => {
-      return true;
-    },
-  },
-};
+import schema from './schema';
 
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
+  schema,
   plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
 });
 
