e3ef571c4b68439e176fd5bc221288ac8230ffe0
jin <kyuj0810@gmail.com>
2023-06-16 17:52:22 +0900

    editprofile1

--- users/createAccount/createAccount.resolvers.js
+++ users/createAccount/createAccount.resolvers.js
@@ -1,15 +1,13 @@
 import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
-import client from '../client';
+import client from '../../client';
 
 export default {
   Mutation: {
-    createdAccount: async (
+    createAccount: async (
       _,
       { firstName, lastName, username, email, password }
     ) => {
       try {
-        // check if username or email are already on DB.
         const existingUser = await client.user.findFirst({
           where: {
             OR: [
@@ -22,9 +20,8 @@ export default {
             ],
           },
         });
-        // has password
         if (existingUser) {
-          throw new Error('This username/email is already taken.');
+          throw new Error('This username/password is already taken.');
         }
         const uglyPassword = await bcrypt.hash(password, 10);
         return client.user.create({
@@ -36,27 +33,8 @@ export default {
             password: uglyPassword,
           },
         });
-
-        // save and return the user
       } catch (e) {
         return e;
-      }
-    },
-    login: async (_, { username, password }) => {
-      const user = await client.user.findFirst({ where: { username } });
-      if (!user) {
-        console.log('test');
-        return {
-          ok: false,
-          error: 'User not found.',
-        };
-      }
-      const passwordOk = await bcrypt.compare(password, user.password);
-      if (!passwordOk) {
-        return {
-          ok: false,
-          error: 'Incorrect password.',
-        };
       }
     },
   },
