a19b059848c174ac8da44ec11115d66a1e400d56
jin <kyuj0810@gmail.com>
2023-06-19 16:36:22 +0900

    recao

--- users/users.utils.js
+++ users/users.utils.js
@@ -19,13 +19,14 @@ export const getUser = async (token) => {
   }
 };
 
-export const protectedResolver =
-  (ourResolver) => (root, args, context, info) => {
+export function protectedResolver(ourResolver) {
+  return function (root, args, context, info) {
     if (!context.loggedInUser) {
       return {
         ok: false,
         error: 'Please log in to perform this action.',
       };
     }
-    return ourResolver(root, arsgs, context, info);
+    return ourResolver(root, args, context, info);
   };
+}
