a00c035604e561a53fb1eaa2decd60883156c9bd
jin <kyuj0810@gmail.com>
2023-07-03 16:06:54 +0900

    4.22 Unfollow User and See Followers

--- users/seeProfile/seeProfile.resolvers.js
+++ users/seeProfile/seeProfile.resolvers.js
@@ -7,6 +7,12 @@ export default {
         where: {
           username,
         },
+        // include : 원하는 사용자 관계를 갖고 올  수 있게 함.
+        include: {
+          following: true,
+          followers: true,
+          _count: true,
+        },
       }),
   },
 };
