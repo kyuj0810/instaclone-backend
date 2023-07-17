f53f91da49eae52f50a7740030870d5d1fd92f45
jin <kyuj0810@gmail.com>
2023-07-17 13:24:35 +0900

    6.20 S3 Photo Upload part Two

--- shared/shared.utils.js
+++ shared/shared.utils.js
@@ -6,3 +6,19 @@ AWS.config.update({
     secretAccessKey: process.env.AWS_SECRET,
   },
 });
+
+export const uploadPhoto = async (file, userId) => {
+  const { filename, createReadStream } = await file;
+  const readStream = createReadStream();
+  const objectName = `${userId}-${Date.now()}-${filename}`;
+  console.log(objectName);
+  const { Location } = await new AWS.S3()
+    .upload({
+      Bucket: 'instaclone-uploads-2023',
+      Key: objectName,
+      ACL: 'public-read', // ACL: object의 프라이버시
+      Body: readStream, //file (stream)
+    })
+    .promise();
+  return Location;
+};
